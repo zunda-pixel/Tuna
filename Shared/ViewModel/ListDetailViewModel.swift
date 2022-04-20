d0706fe916627eede6a9ebf69162f926ccac757a
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-04-21 00:26:26 +0900

    ListDetailViewModelを実装

---
+++ Shared/ViewModel/ListDetailViewModel.swift
@@ -0,0 +1,76 @@
+//
+//  ListDetailViewModel.swift
+//  Tuna
+//
+//  Created by zunda on 2022/04/21.
+//
+
+import Foundation
+import Sweet
+
+@MainActor protocol ListDetailViewModelProtocol: TweetsViewModelProtocol {
+  var list: Sweet.ListModel { get }
+}
+
+@MainActor final class ListDetailViewModel: ListDetailViewModelProtocol {
+  @Published var timelines: [String] = []
+  @Published var allTweets: [Sweet.TweetModel] = []
+  @Published var allUsers: [Sweet.UserModel] = []
+  @Published var allMedias: [Sweet.MediaModel] = []
+  @Published var allPolls: [Sweet.PollModel] = []
+  @Published var allPlaces: [Sweet.PlaceModel] = []
+
+  var paginationToken: String? = nil
+
+  let list: Sweet.ListModel
+
+  init(list: Sweet.ListModel) {
+    self.list = list
+  }
+
+  func fetchTweets() async {
+    do {
+      let sweet = try await Sweet()
+      let listResponse = try await sweet.fetchListTweets(listID: list.id, paginationToken: paginationToken)
+
+      paginationToken = listResponse.meta?.nextToken
+
+      let tweetIDs = listResponse.tweets.map(\.id)
+
+      if tweetIDs.isEmpty {
+        return
+      }
+
+      let tweetResponse = try await sweet.lookUpTweets(by: tweetIDs)
+
+      tweetResponse.tweets.forEach {
+        allTweets.appendIfNotContains($0)
+      }
+
+      tweetResponse.relatedTweets.forEach {
+        allTweets.appendIfNotContains($0)
+      }
+
+      tweetResponse.users.forEach {
+        allUsers.appendIfNotContains($0)
+      }
+
+      tweetResponse.polls.forEach {
+        allPolls.appendIfNotContains($0)
+      }
+
+      tweetResponse.medias.forEach {
+        allMedias.appendIfNotContains($0)
+      }
+
+      tweetResponse.places.forEach {
+        allPlaces.appendIfNotContains($0)
+      }
+
+      timelines.append(contentsOf: tweetIDs)
+    } catch {
+      print(error)
+      fatalError()
+    }
+  }
+}
