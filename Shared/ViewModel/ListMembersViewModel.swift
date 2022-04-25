f139ecce3f1ffde2251a6b880add55af4cb69793
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-04-25 23:58:42 +0900

    ListMembersViewModelを実装

---
+++ Shared/ViewModel/ListMembersViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ListMembersViewModel.swift
+//  Tuna
+//
+//  Created by zunda on 2022/04/25.
+//
+
+import Foundation
+import Sweet
+
+@MainActor final class ListMembersViewModel: UsersViewModelProtocol {
+  var users: [Sweet.UserModel]
+  let listID: String
+
+  init(listID: String, users: [Sweet.UserModel] = []) {
+    self.listID = listID
+    self.users = users
+  }
+
+  func fetchUsers() async {
+    do {
+      let sweet = try await Sweet()
+      let listResponse = try await sweet.fetchAddedUsersToList(listID: listID)
+      users = listResponse.users
+    } catch {
+      print(error)
+    }
+  }
+}
