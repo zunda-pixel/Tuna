5e55fe1f5bac3a9eabdea75d5a93120cfff02c8d
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-05-25 23:28:19 +0900

    TweetsViewProtocolをCoreDataも持てるように

--- Shared/ViewModel/TweetsViewModel.swift
+++ Shared/ViewModel/TweetsViewModel.swift
@@ -7,62 +7,145 @@
 
 import Foundation
 import Sweet
+import CoreData
 
-@MainActor protocol TweetsViewProtocol: ObservableObject {
+@MainActor protocol TweetsViewProtocol: NSFetchedResultsControllerDelegate, ObservableObject {
+  var isPresentedTweetToolbar: Bool { get set }
+  var latestTapTweetID: String? { get set }
+
+  var error: Error? { get }
   var didError: Bool { get set }
-  var error: Error? { get set }
-  var timelines: [String] { get set }
 
-  var allTweets: [Sweet.TweetModel] { get set }
-  var allUsers: [Sweet.UserModel] { get set }
-  var allMedias: [Sweet.MediaModel] { get set }
-  var allPolls: [Sweet.PollModel] { get set }
-  var allPlaces: [Sweet.PlaceModel] { get }
+  var viewContext: NSManagedObjectContext { get }
+
+  var timelines: [Timeline] { get }
+  var fetchTimelineController: NSFetchedResultsController<Timeline> { get }
+
+  var showTweets: [Tweet] { get }
+  var fetchShowTweetController: NSFetchedResultsController<Tweet> { get }
+
+  var allTweets: [Tweet] { get }
+  var fetchTweetController: NSFetchedResultsController<Tweet> { get }
+
+  var allUsers: [User] { get }
+  var fetchUserController: NSFetchedResultsController<User> { get }
+
+  var allMedias: [Media] { get }
+  var fetchMediaController: NSFetchedResultsController<Media> { get }
+
+  var allPolls: [Poll] { get }
+  var fetchPollController: NSFetchedResultsController<Poll> { get }
+
+  var allPlaces: [Place] { get }
+  var fetchPlaceController: NSFetchedResultsController<Place> { get }
+
+  func addTimeline(tweet: Sweet.TweetModel, userID: String) throws
+  func addTweet(_ tweet: Sweet.TweetModel) throws
+  func addUser(_ user: Sweet.UserModel) throws
+  func addPlace(_ place: Sweet.PlaceModel) throws
+  func addPoll(_ poll: Sweet.PollModel) throws
+  func addMedia(_ media: Sweet.MediaModel) throws
 
   func getTweet(_ tweetID: String?) -> Sweet.TweetModel?
   func getPoll(_ pollID: String?) -> Sweet.PollModel?
   func getUser(_ userID: String?) -> Sweet.UserModel?
   func getMedias(_ mediaIDs: [String]?) -> [Sweet.MediaModel]
   func getPlace(_ placeID: String?) -> Sweet.PlaceModel?
+
   func getTweetCellViewModel(_ tweetID: String) -> TweetCellViewModel
-  func fetchTweets() async
+
+  func fetchTweets(first firstTweetID: String?, last lastTweetID: String?, paginationToken: String?) async
+  func updateTimeLine()
 }
 
 extension TweetsViewProtocol {
-  func getTweetCellViewModel(_ tweetID: String) -> TweetCellViewModel {
-    let tweetModel = getTweet(tweetID)!
+  var timelines: [Timeline] { fetchTimelineController.fetchedObjects ?? [] }
+  var showTweets: [Tweet] { fetchShowTweetController.fetchedObjects ?? [] }
+  var allTweets: [Tweet] { fetchTweetController.fetchedObjects ?? [] }
+  var allUsers: [User] { fetchUserController.fetchedObjects ?? [] }
+  var allMedias: [Media] { fetchMediaController.fetchedObjects ?? [] }
+  var allPolls: [Poll] { fetchPollController.fetchedObjects ?? [] }
+  var allPlaces: [Place] { fetchPlaceController.fetchedObjects ?? [] }
 
-    let retweetTweetModel = getTweet(tweetModel.referencedTweet?.id)
+  func addPlace(_ place: Sweet.PlaceModel) throws {
+    if let firstPlace = allPlaces.first(where: { $0.id == place.id }) {
+      firstPlace.setPlaceModel(place)
+    } else {
+      let newPlace = Place(context: viewContext)
+      newPlace.setPlaceModel(place)
+    }
 
-    let authorUser = getUser(tweetModel.authorID!)!
+    try viewContext.save()
+  }
 
-    let retweetUser = getUser(retweetTweetModel?.authorID)
+  func addTweet(_ tweet: Sweet.TweetModel) throws {
+    if let firstTweet = allTweets.first(where: { $0.id == tweet.id }) {
+      try firstTweet.setTweetModel(tweet)
+    } else {
+      let newTweet = Tweet(context: viewContext)
+      try newTweet.setTweetModel(tweet)
+    }
 
-    let medias = getMedias(tweetModel.attachments?.mediaKeys)
+    try viewContext.save()
+  }
 
-    let poll = getPoll(tweetModel.attachments?.pollID)
+  func addPoll(_ poll: Sweet.PollModel) throws {
+    if let firstPoll = allPolls.first(where: { $0.id == poll.id }) {
+      try firstPoll.setPollModel(poll)
+    } else {
+      let newPoll = Poll(context: viewContext)
+      try newPoll.setPollModel(poll)
+    }
+    try viewContext.save()
+  }
 
-    let place = getPlace(tweetModel.geo?.placeID)
+  func addUser(_ user: Sweet.UserModel) throws {
+    if let firstUser = allUsers.first(where: { $0.id == user.id }) {
+      try firstUser.setUserModel(user)
+    } else {
+      let newUser = User(context: viewContext)
+      try newUser.setUserModel(user)
+    }
+    try viewContext.save()
+  }
 
-    let viewModel: TweetCellViewModel = .init(tweet: tweetModel, retweet: retweetTweetModel, author: authorUser, retweetUser: retweetUser, medias: medias, poll: poll, place: place)
+  func addMedia(_ media: Sweet.MediaModel) throws {
+    if let firstMedia = allMedias.first(where: { $0.key == media.key }) {
+      firstMedia.setMediaModel(media)
+    } else {
+      let newMedia = Media(context: viewContext)
+      newMedia.setMediaModel(media)
+    }
+    try viewContext.save()
+  }
 
-    return viewModel
+  func addTimeline(tweet: Sweet.TweetModel, userID: String) throws {
+    if timelines.contains(where: { $0.tweetID == tweet.id }) {
+      return
+    }
+
+    let newTimeline = Timeline(context: viewContext)
+    newTimeline.tweetID = tweet.id
+    newTimeline.ownerID = userID
+    try viewContext.save()
   }
 
   func getPlace(_ placeID: String?) -> Sweet.PlaceModel? {
     guard let placeID = placeID else { return nil }
 
-    let firstPlace = allPlaces.first(where: { $0.id == placeID })
+    guard let firstPlace = allPlaces.first(where: { $0.id == placeID }) else {
+      return nil
+    }
 
-    return firstPlace
+    return .init(place: firstPlace)
   }
 
   func getPoll(_ pollID: String?) -> Sweet.PollModel? {
     guard let pollID = pollID else { return nil }
 
-    let firstPoll = allPolls.first(where: { $0.id == pollID })
+    guard let firstPoll = allPolls.first(where: { $0.id == pollID }) else { return nil }
 
-    return firstPoll
+    return .init(poll: firstPoll)
   }
 
   func getMedias(_ mediaIDs: [String]?) -> [Sweet.MediaModel] {
@@ -70,26 +153,52 @@ extension TweetsViewProtocol {
       return []
     }
 
-    let medias = allMedias.filter { mediaIDs.contains($0.key) }
+    let medias = allMedias.filter({ mediaIDs.contains($0.key!) })
 
-    return medias
+    return medias.map { .init(media: $0) }
   }
 
   func getUser(_ userID: String?) -> Sweet.UserModel? {
     guard let userID = userID else { return nil }
 
-    let user = allUsers.first(where: { $0.id == userID })
+    guard let firstUser = allUsers.first(where: { $0.id == userID }) else { return nil }
 
-    return user
+    return .init(user: firstUser)
   }
 
   func getTweet(_ tweetID: String?) -> Sweet.TweetModel? {
-    guard let tweetID = tweetID else {
-      return nil
-    }
+    guard let tweetID = tweetID else { return nil }
 
-    let retweetTweet = allTweets.first { $0.id == tweetID }
+    guard let tweet = allTweets.first(where: { $0.id == tweetID }) else { return nil }
 
-    return retweetTweet
+    return .init(tweet: tweet)
+  }
+
+  func getTweetCellViewModel(_ tweetID: String) -> TweetCellViewModel {
+    let tweet = getTweet(tweetID)!
+
+    let retweet = getTweet(tweet.referencedTweet?.id)
+
+    let author = getUser(tweet.authorID!)!
+
+    let retweetUser = getUser(retweet?.authorID)
+
+    let medias = getMedias(tweet.attachments?.mediaKeys)
+
+    let poll = getPoll(tweet.attachments?.pollID)
+
+    let place = getPlace(tweet.geo?.placeID)
+
+    let viewModel: TweetCellViewModel = .init(
+      tweet: tweet, retweet: retweet, author: author,
+      retweetUser: retweetUser, medias: medias, poll: poll, place: place)
+
+    return viewModel
+  }
+
+  func updateTimeLine() {
+    let tweetIDs = timelines.compactMap(\.tweetID)
+    fetchShowTweetController.fetchRequest.predicate = NSPredicate(format: "id IN %@", tweetIDs)
   }
 }
+
