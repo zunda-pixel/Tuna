96f6cc6646a7274fb38907bfb69cfece704b45c5
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-06-08 00:32:58 +0900

    エラーになってしまう部分をコメントアウト

--- Shared/View/User/UserView.swift
+++ Shared/View/User/UserView.swift
@@ -66,20 +66,21 @@ struct UserView: View {
         }
         .pickerStyle(.segmented)
 
-        TabView(selection: $selection) {
-          let userTimelineViewModel: UserTimelineViewModel = .init(userID: user.id, viewContext: viewContext)
-          TweetsView(viewModel: userTimelineViewModel)
-            .tag(TweetTab.tweet)
-
-          let userMentionsViewModel: UserMentionsViewModel = .init(userID: user.id, viewContext: viewContext)
-          TweetsView(viewModel: userMentionsViewModel)
-            .tag(TweetTab.mention)
-
-          let likeViewModel: LikesViewModel = .init(userID: user.id, viewContext: viewContext)
-          TweetsView(viewModel: likeViewModel)
-            .tag(TweetTab.like)
-        }
-        .tabViewStyle(.page(indexDisplayMode: .never))
+        // TODO なぜかエラーになる
+//        TabView(selection: $selection) {
+//          let userTimelineViewModel: UserTimelineViewModel = .init(userID: user.id, viewContext: viewContext)
+//          TweetsView(viewModel: userTimelineViewModel)
+//            .tag(TweetTab.tweet)
+//
+//          let userMentionsViewModel: UserMentionsViewModel = .init(userID: user.id, viewContext: viewContext)
+//          TweetsView(viewModel: userMentionsViewModel)
+//            .tag(TweetTab.mention)
+//
+//          let likeViewModel: LikesViewModel = .init(userID: user.id, viewContext: viewContext)
+//          TweetsView(viewModel: likeViewModel)
+//            .tag(TweetTab.like)
+//        }
+//        .tabViewStyle(.page(indexDisplayMode: .never))
       }
     }
   }
