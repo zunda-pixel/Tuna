685fb0b564d4f7d05b7ecb4d43398fb6ae2d3b1f
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-06-20 15:21:25 +0900

    画像の拡大縮小できるように

--- Shared/View/ScrollImagesView.swift
+++ Shared/View/ScrollImagesView.swift
@@ -10,30 +10,60 @@ import Sweet
 import Kingfisher
 
 struct ScrollImagesView: View {
+  let medias: [Sweet.MediaModel]
+  @State var scales: [String: Double] = [:]
+  @Binding var selectedMedia: Sweet.MediaModel
   @Environment(\.dismiss) var dismiss
 
-  let medias: [Sweet.MediaModel]
+  init(medias: [Sweet.MediaModel], selectedMedia: Binding<Sweet.MediaModel>) {
+    self.medias = medias
+    self._selectedMedia = selectedMedia
 
-  @Binding var selectedMedia: Sweet.MediaModel
+    medias.forEach { media in
+      scales[media.id] = 1
+    }
+  }
 
   var body: some View {
-    TabView(selection: $selectedMedia) {
-      ForEach(medias) { media in
-        let mediaURL = media.url ?? media.previewImageURL!
-        ZStack {
-          Color.black
-            .ignoresSafeArea()
-          KFImage(mediaURL)
-            .resizable()
-            .scaledToFit()
-            .onTapGesture {
-              dismiss()
+    ZStack {
+      Color.black
+        .ignoresSafeArea()
+      TabView(selection: $selectedMedia) {
+        ForEach(medias) { media in
+          let mediaURL = media.url ?? media.previewImageURL!
+          let magnification = MagnificationGesture()
+            .onChanged { value in
+              scales[media.id] = value
             }
+            .onEnded { value in
+              if value < 1 {
+                withAnimation(.easeInOut) {
+                  scales[media.id] = 1
+                }
+              }
+            }
+          ZStack {
+            Color.black
+            KFImage(mediaURL)
+              .resizable()
+              .scaleEffect(scales[media.id] ?? 1)
+              .scaledToFit()
+              .onTapGesture(count: 2) {
+                withAnimation(.easeInOut) {
+                  scales[media.id] = 1
+                }
+              }
+              .onTapGesture(count: 1) {
+                dismiss()
+              }
+              .gesture(magnification)
+              .padding(.horizontal)
+          }
+            .gesture(magnification)
+            .tag(media)
         }
-        .tag(media)
       }
+      .tabViewStyle(.page)
     }
-    .ignoresSafeArea()
-    .tabViewStyle(.page)
   }
 }
