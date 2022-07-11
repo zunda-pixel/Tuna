53b3127e9bb934050ac1ab1c72ed1416c96757ff
zunda <47569369+zunda-pixel@users.noreply.github.com>
2022-07-11 18:00:44 +0900

    不要な値を削除

--- Shared/ViewModel/Search/SearchViewModel.swift
+++ Shared/ViewModel/Search/SearchViewModel.swift
@@ -11,19 +11,17 @@ import Foundation
   associatedtype TweetsViewModel: SearchTweetsViewModel
   associatedtype UsersViewModel: SearchUsersViewModel
 
-  var searchUserIDs: [String] { get }
   var tweetsViewModel: TweetsViewModel { get set }
   var usersViewModel: UsersViewModel { get set }
 }
 
 @MainActor final class SearchViewModel: NSObject, SearchViewProtocol {
-  var searchUserIDs: [String] = []
   @Published var tweetsViewModel: SearchTweetsViewModel
   @Published var usersViewModel: SearchUsersViewModel
+
   var searchText: String = ""
 
   init(userID: String) {
-
     self.tweetsViewModel = .init(userID: userID)
     self.usersViewModel = .init(userID: userID)
   }
